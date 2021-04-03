30f54025cb6e02f99b470d46b1beab1af6e38cab
Alexey <krivonosov2004@mail.ru>
2021-04-03 16:42:24 +0500

    login form.

--- test.py
+++ test.py
@@ -1,17 +1,43 @@
 from flask import Flask, render_template
+from werkzeug.utils import redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My little strange password that i don`t understand"
 
 
-@app.route('/index')
+@app.route('/')
 def test():
-    return render_template('test.html')
+    return render_template('base.html')
 
 
-@app.route('/index1')
+@app.route('/success')
 def third():
     return 'success'
     # return render_template('3.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
