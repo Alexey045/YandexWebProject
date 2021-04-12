97aeb868e0804704152c1fe7f4e7d7a54a723d60
Alexey <krivonosov2004@mail.ru>
2021-04-12 18:18:20 +0500

    idk what is it:/

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 
@@ -6,21 +6,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def main():
-    """db_session.global_init("db/blogs.db")
-    user1 = User()
-    user1.surname = "Scott"
-    user1.name = "Ridley"
-    user1.age = 21
-    user1.position = "captain"
-    user1.speciality = "research engineer"
-    user1.address = "module_1"
-    user1.email = "scott_chief@mars.org"
-    db_sess = db_session.create_session()
-    db_sess.add_all([user1])
-    db_sess.commit()
-    # app.run()"""
+@app.route('/')
+def point():
+    render_template('base.html')
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
+
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
+
+"""from data import db_session
+from data.users import User"""
 from forms import RegisterForm, LoginForm
-from data import db_session
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 
 # from data.users import User
@@ -20,9 +23,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "My little strange password that i don`t understand"
 
 
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+
 @app.route('/')
 def base():
-    return render_template('base.html', title='Главная страница')
+    return render_template('main.html', title='Главная страница')
+
+
+"""@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)"""
 
 
 @app.route('/success')
@@ -46,6 +59,32 @@ def registration():
     return render_template('registration.html', title='Регистрация', form=form)
 
 
+"""
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)"""
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -53,6 +92,29 @@ def login():
         # print(request.form.get('remember_me')) y - check true. None - check false
         return redirect('/success')
     return render_template('login.html', title='Авторизация', form=form)
+
+
+"""
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)"""
+
+"""
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")"""
 
 
 @app.route('/about')
